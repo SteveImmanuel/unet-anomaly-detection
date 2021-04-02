9d734a14c6729c0f947f0a6475649028677210c0
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-04-02 12:06:31 +0700

    Update config file

--- anomaly_detector/config.py
+++ anomaly_detector/config.py
@@ -1,5 +1,25 @@
+#Model configuration
 DIM = (160, 120, 1)
 SEQ_LEN = 5
 BATCH_SIZE = 32
-MODEL_NAME = f'avenue_single_unet_2scale'
+MODEL_NAME = 'ped1_single_unet_2scale'
 FRAME_SIZE_MULTIPLIER = 2.5
+
+#Dataset configuration
+DATASET = 'ped1'
+TRAIN_PATH = 'dataset/ped1/Train'
+VALIDATION_PATH = 'dataset/ped1/Validation'
+TEST_PATH = 'dataset/ped1/Test'
+
+#Callback configuration
+UPDATE_FREQ = 50
+CHECKPOINT_PERIOD = 3
+STOP_PATIENCE = 10
+REDUCE_PATIENCE = 4
+DISPLAY_WHILE_TRAINING = False
+OUTPUT_TRAINING_IMAGES = 'training_images'
+
+#Training configuration
+LEARNING_RATE = 1e-3
+EPSILON = 1e-7
+EPOCHS = 100
