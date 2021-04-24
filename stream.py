58d1a9190abaad89ff7ca26ecf57122853162bc4
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-04-24 19:44:02 +0700

    Implement streaming from video file

--- stream.py
+++ stream.py
@@ -2,12 +2,16 @@
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-# from anomaly_detector.network.model import get_model
+import time
+import logging
 from anomaly_detector.config import MODEL_NAME, DIM, BATCH_SIZE, SEQ_LEN, TRAIN_PATH, VALIDATION_PATH, TEST_PATH, LEARNING_RATE, EPSILON, EPOCHS
-from anomaly_detector.threads import Predictor, Reader, Evaluator
+from anomaly_detector.threads import Predictor, Reader, Visualizer
 from queue import Queue
 from threading import Event
-import time
+
+logging.basicConfig()
+logger = logging.getLogger('Stream')
+logger.setLevel(logging.INFO)
 
 read_event = Event()
 predict_event = Event()
@@ -17,21 +21,19 @@ model_output = Queue()
 reader = Reader('01.mp4', model_input, BATCH_SIZE, SEQ_LEN, DIM, read_event)
 predictor = Predictor(f'pretrained/{MODEL_NAME}/{MODEL_NAME}_end_train', model_input, model_output,
                       read_event, predict_event)
-evaluator = Evaluator(model_output, predict_event)
+visualizer = Visualizer(model_output, predict_event, 30)
 
-print('Initializing reader')
 reader.start()
-print('Initializing predictor')
 predictor.start()
-print('Initializing evaluator')
-evaluator.start()
+visualizer.start()
 
-reader.join()
-predictor.join()
-evaluator.join()
-# try:
-#     while True:
-#         time.sleep(0.01)
-# except KeyboardInterrupt:
-#     print('stopping thread')
-#     finish_event.set()
+try:
+    while True:
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    logger.info('Stopping thread')
+    read_event.set()
+    predict_event.set()
+    reader.join()
+    predictor.join()
+    visualizer.join()
