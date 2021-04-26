66d0cebf3d10710be46208741bad5999828e0c9e
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-04-26 23:02:25 +0700

    Display error map

--- anomaly_detector/threads/visualizer.py
+++ anomaly_detector/threads/visualizer.py
@@ -21,14 +21,19 @@ class Visualizer(Thread):
     def run(self):
         try:
             cv2.namedWindow('Prediction', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('Prediction', 160 * 3 * 3, 120 * 3)
 
             while not (self.predict_event.is_set() and self.model_output.empty()):
                 if not self.model_output.empty():
                     logger.debug('Receive item')
                     raw_result = self.model_output.get()
+
+                    g_truth = np.repeat(raw_result['g_truth'], 3, axis=-1)
+                    prediction = np.repeat(raw_result['prediction'], 3, axis=-1)
+
                     for i in range(len(raw_result['prediction'])):
                         combined_image = np.concatenate(
-                            (raw_result['prediction'][i], raw_result['g_truth'][i]), axis=1)
+                            (g_truth[i], prediction[i], raw_result['error_map'][i]), axis=1)
                         cv2.imshow('Prediction', combined_image)
                         cv2.waitKey(1000 // self.fps)
         except Exception as e:
