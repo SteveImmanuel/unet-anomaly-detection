180892979f98ca8c2750089dc7f8c3d930a293c8
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-04-26 23:02:07 +0700

    Fix typing hint

--- anomaly_detector/network/loss_function.py
+++ anomaly_detector/network/loss_function.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
-from nptyping import ndarray
+from nptyping import NDArray
 from tensorflow.keras import backend as K
 
 
-def l1_loss(y_true: ndarray, y_pred: ndarray):
+def l1_loss(y_true: NDArray, y_pred: NDArray):
     error = y_true - y_pred
     error = K.abs(error)
     sum_error = K.sum(error, axis=(1, 2, 3))
@@ -11,7 +11,7 @@ def l1_loss(y_true: ndarray, y_pred: ndarray):
     return l1_loss
 
 
-def l2_loss(y_true: ndarray, y_pred: ndarray):
+def l2_loss(y_true: NDArray, y_pred: NDArray):
     error = y_true - y_pred
     sqr_error = K.square(error)
     sum_sqr_error = K.sum(sqr_error, axis=(1, 2, 3))
@@ -19,7 +19,7 @@ def l2_loss(y_true: ndarray, y_pred: ndarray):
     return l2_loss
 
 
-def gradient_loss(y_true: ndarray, y_pred: ndarray):
+def gradient_loss(y_true: NDArray, y_pred: NDArray):
     dy_y_true, dx_y_true = tf.image.image_gradients(y_true)
     dy_y_pred, dx_y_pred = tf.image.image_gradients(y_pred)
     diff = K.abs(dx_y_pred - dx_y_true) + K.abs(dy_y_pred - dy_y_true)
@@ -29,14 +29,14 @@ def gradient_loss(y_true: ndarray, y_pred: ndarray):
     return gradient_loss
 
 
-def total_loss(y_true: ndarray, y_pred: ndarray):
+def total_loss(y_true: NDArray, y_pred: NDArray):
     g_loss = gradient_loss(y_true, y_pred)
     p_loss = l2_loss(y_true, y_pred)
     return p_loss + g_loss
 
 
 def l2_loss_with_weight(weight: int):
-    def l2_loss(y_true: ndarray, y_pred: ndarray):
+    def l2_loss(y_true: NDArray, y_pred: NDArray):
         error = y_true - y_pred
         sqr_error = K.square(error)
         sum_sqr_error = K.sum(sqr_error, axis=(1, 2, 3))
--- anomaly_detector/utils/evaluation.py
+++ anomaly_detector/utils/evaluation.py
@@ -1,7 +1,11 @@
+import numpy as np
 from typing import List, Tuple, Dict
-from nptyping import ndarray
+from nptyping import NDArray
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
+from tensorflow.keras import Input
+from tensorflow.keras.layers import Conv2D, Lambda
+from tensorflow.keras.initializers import Constant
 
 
 def get_error_patcher(
@@ -27,7 +31,7 @@ def get_error_patcher(
     return error_patcher
 
 
-def smoothen_loss(loss_list: ndarray, strength: float) -> ndarray:
+def smoothen_loss(loss_list: NDArray, strength: float) -> NDArray:
     last_smoothed = loss_list[0]
 
     smoothed_list = []
@@ -40,28 +44,28 @@ def smoothen_loss(loss_list: ndarray, strength: float) -> ndarray:
 
 
 def calculate_loss(error_patcher: Model,
-                   prediction: ndarray,
-                   g_truth: ndarray,
+                   prediction: NDArray,
+                   g_truth: NDArray,
                    smooth_strength: float = 0.5,
                    upper_limit_loss: float = 9999,
                    min_loss: float = None,
-                   max_loss: float = None) -> Tuple[ndarray, ndarray]:
+                   max_loss: float = None) -> Tuple[NDArray, NDArray]:
     raw_losses, losses = error_patcher.predict(np.square(prediction - g_truth))
     losses = np.clip(losses, a_min=0, a_max=upper_limit_loss)
     if min_loss == None:
         min_loss = np.min(losses)
     if max_loss == None:
         max_loss = np.max(losses)
-    print(min_loss, max_loss)
     losses = smoothen_loss(losses, smooth_strength)
     losses = 1 - (losses - min_loss) / (max_loss - min_loss)
-    return losses, raw_losses
+    return raw_losses, losses
 
 
-def draw_bounding_box(image: ndarray,
-                      index: int,
-                      pool_size: Tuple[int, int] = (16, 16),
-                      strides: Tuple[int, int] = (1, 1)):
+def draw_bounding_box(
+    image: NDArray,
+    index: int,
+    pool_size: Tuple[int, int] = (16, 16),
+    strides: Tuple[int, int] = (1, 1)) -> None:
     top_left_row = index[0] * strides[0]
     top_left_col = index[1] * strides[1]
     image[top_left_row:top_left_row + pool_size[0], top_left_col] = np.array([1, 0, 0])
@@ -72,12 +76,32 @@ def draw_bounding_box(image: ndarray,
           top_left_col:top_left_col + pool_size[1]] = np.array([1, 0, 0])
 
 
-def locate_anomaly(pred_dict: Dict,
-                   g_truth_dict: Dict,
-                   raw_losses_dict: Dict,
-                   losses_dict: Dict,
+def locate_anomaly(prediction: NDArray,
+                   g_truth: NDArray,
+                   raw_losses: NDArray,
+                   losses: NDArray,
                    pool_size: Tuple[int, int],
-                   threshold: float = 0.5) -> Dict:
+                   threshold: float = 0.5) -> NDArray:
+
+    error_map = np.abs(prediction - g_truth)
+    if error_map.shape[-1] != 3:
+        error_map = np.repeat(error_map, 3, axis=-1)
+
+    for i in range(len(error_map)):
+        if losses[i] >= threshold:
+            continue
+
+        index = np.unravel_index(np.argmax(raw_losses[i], axis=None), raw_losses[i].shape)
+        draw_bounding_box(error_map[i], (index[0], index[1]), pool_size)
+    return error_map
+
+
+def locate_all_anomaly(pred_dict: Dict,
+                       g_truth_dict: Dict,
+                       raw_losses_dict: Dict,
+                       losses_dict: Dict,
+                       pool_size: Tuple[int, int],
+                       threshold: float = 0.5) -> Dict:
     error_map_dict = {}
 
     for key in pred_dict.keys():
