a11db95873f253a86e1120cc1b8e9a56feef84f6
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-05-02 20:50:42 +0700

    Update window resolution

--- anomaly_detector/threads/visualizer.py
+++ anomaly_detector/threads/visualizer.py
@@ -21,7 +21,7 @@ class Visualizer(Thread):
     def run(self):
         try:
             cv2.namedWindow('Prediction', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Prediction', 160 * 3 * 3, 120 * 3)
+            cv2.resizeWindow('Prediction', 160 * 7, 120 * 7)
 
             while not (self.predict_event.is_set() and self.model_output.empty()):
                 if not self.model_output.empty():
@@ -32,12 +32,17 @@ class Visualizer(Thread):
                     prediction = np.repeat(raw_result['prediction'], 3, axis=-1)
 
                     for i in range(len(raw_result['prediction'])):
-                        combined_image = np.concatenate(
-                            (g_truth[i], prediction[i], raw_result['error_map'][i]), axis=1)
+                        combined_image = np.zeros(160 * 120 * 4 * 3).reshape(240, 320, 3)
+                        combined_image += 0.25
+                        combined_image[:120, :160] = g_truth[i]
+                        combined_image[:120, 160:] = prediction[i]
+                        combined_image[120:, 80:240] = raw_result['error_map'][i]
+
                         cv2.imshow('Prediction', combined_image)
                         cv2.waitKey(1000 // self.fps)
         except Exception as e:
             logger.error(e)
         finally:
+            input('Press enter to continue...')
             cv2.destroyAllWindows()
             logger.info('Completed')
