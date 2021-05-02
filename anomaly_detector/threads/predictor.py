b9d84ef1eb523f45572c48303a192a66e7c8f2ef
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-05-02 12:01:31 +0700

    Create model outside thread to reduce memory

--- anomaly_detector/threads/predictor.py
+++ anomaly_detector/threads/predictor.py
@@ -3,10 +3,9 @@ import time
 import logging
 from threading import Thread, Event
 from queue import Queue
-from typing import List
-from tensorflow.keras.models import Model, load_model
-from anomaly_detector.network.loss_function import total_loss
-from anomaly_detector.utils.evaluation import get_error_patcher, locate_anomaly, calculate_loss
+from typing import List, Tuple
+from tensorflow.keras.models import Model
+from anomaly_detector.utils.evaluation import locate_anomaly, calculate_loss
 from anomaly_detector.config import DIM, POOL_SIZE, SMOOTH_STRENGTH
 
 root_logger = logging.getLogger('Stream')
@@ -14,18 +13,19 @@ logger = root_logger.getChild('Predictor')
 
 
 class Predictor(Thread):
-    def __init__(self, model_path: str, model_input: Queue, model_output: Queue, read_event: Event,
-                 predict_event: Event):
+    def __init__(self, model: Model, error_patcher: Model, model_input: Queue, model_output: Queue,
+                 read_event: Event, predict_event: Event):
         Thread.__init__(self)
         self.model_input = model_input
         self.model_output = model_output
         self.read_event = read_event
         self.predict_event = predict_event
-        self.error_patcher = get_error_patcher((DIM[1], DIM[0], DIM[2]), POOL_SIZE)
-
-        model = load_model(model_path, custom_objects={'total_loss': total_loss})
-        self.model = Model(inputs=model.input, outputs=model.output[0])
+        self.error_patcher = error_patcher
+        self.model = model
         logger.info('Initialization complete')
+
+    def set_normalization_param(self, param: Tuple[float, float]):
+        self.normalization_param = param
 
     def run(self):
         elapsed = -time.time()
@@ -41,6 +41,8 @@ class Predictor(Thread):
                     raw_losses, losses = calculate_loss(self.error_patcher,
                                                         y,
                                                         x['target'],
+                                                        min_loss=self.normalization_param[0],
+                                                        max_loss=self.normalization_param[1],
                                                         smooth_strength=SMOOTH_STRENGTH)
                     error_map = locate_anomaly(y, x['target'], raw_losses, losses, POOL_SIZE, 0.55)
                     logger.debug(error_map.shape)
